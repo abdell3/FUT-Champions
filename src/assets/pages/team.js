8f45db70ee1e5a7ca774638302996c9ca6eb51f7
abdellah <abdo.abdell.2000@gmail.com>
2024-11-27 15:43:53 +0100

    Fetch data json et modifier la section de squad

--- src/assets/pages/team.js
+++ src/assets/pages/team.js
@@ -1,25 +1,58 @@
-let search = document.querySelector('.search-box');
+fetch("players.json")
+      .then((response) => response.json())
+      .then((data) => displayPlayers(data.players));
+    
+    function displayPlayers(info){
+        const allPlayers = document.getElementById("players-container");
+        allPlayers.innerHTML = info.map((item)=>{
+            return `<div class="players-contain" >
 
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
-    menu.classList.remove('active');
-}
+            <div  class ="player-contain1">
+                <div class="d-flex flex-">
+                <div >
+                <h6>${item.position}</h6>
+                <h4>${item.rating}</h4>
+                </div>
+                <img src=${item.photo} alt="">
+                </div>
+              <h6>${item.name}</h6>
 
-let menu = document.querySelector('.navbar');
+        if (player.position == "GK") {
+            html +=   <div>
 
-document.querySelector('#menu-icon').onclick = () => {
-    menu.classList.toggle('active');
-    search.classList.remove('active');
-}
-// hide menu and search box on scroll
-window.onscroll = () => {
-    menu.classList.remove('active');
-    search.classList.remove('active');
-}
-//header
+                <span>${item.rating}+ player.rating + </span>
+                <span>${item.diving}+ player.diving+ </span>
+                <span>${item.handling}+ player.handling + </span>
+                <span>${item.kicking}+ player.kicking + </span>
+                <span>${item.reflexes}+ player.reflexes + </span>
+                <span>${item.speed}+ player.speed +</span>
+                <span>${item.positioning}+ player.positioning+</span>
 
-let header = document.querySelector('header');
+            </div>
 
-window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow', window.scrollY>0);
-});
+        } else {
+            html +=   <div>
+
+                <span>${item.pace}+ player.pace + </span>
+                <span>${item.shooting}+ player.shooting + </span>
+                <span>${item.passing}+ player.passing + </span>
+                <span>${item.dribbling}+ player.dribbling + </span>
+                <span>${item.defending}+ player.defending + </span>
+                <span>${item.physical}+ player.physical + </span>
+            </div>
+        }
+
+        html +=
+
+              <p>${item.nationality}+ player.nationality + </p> 
+              <div style="padding-bottom: 30px;">
+              <img style="border-radius: 50%; width:20px; height:20px"  src=${item.flag} alt="">
+              <img style="border-radius: 50%; width:20px; height:20px"  src=${item.logo} alt="">
+              </div>
+
+        </div>
+    </div>`
+    
+        })
+        .join( ``);
+    }
